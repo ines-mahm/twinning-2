12588fa4bcd40a5756d7dde3adb65393b993b4c6
julia-langer <julia.langer@edu.fh-joanneum.at>
2020-04-28 17:21:58 +0200

    changed grid number

--- Assets/Scripts/AddCards.cs
+++ Assets/Scripts/AddCards.cs
@@ -15,7 +15,7 @@ public class AddCards : MonoBehaviour
 
     void Awake()
 	{
-        for (int i=0; i < 12; i++)
+        for (int i=0; i < 16; i++)
 		{
 			GameObject card = Instantiate(card_btn);
 			card.name = "" + i;
